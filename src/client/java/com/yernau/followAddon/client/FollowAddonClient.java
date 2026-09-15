a3bac6eed54f6ee49151f1b7313e0d85da1be32b
Andrew <asyermak@gmail.com>
2026-09-15 11:23:02 +0200

    Gradle build

---
+++ src/client/java/com/yernau/followAddon/client/FollowAddonClient.java
@@ -0,0 +1,10 @@
+package com.yernau.followAddon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FollowAddonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/yernau/followAddon/FollowAddon.java
@@ -0,0 +1,10 @@
+package com.yernau.followAddon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FollowAddon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
